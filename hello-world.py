4da53ce3f16ca8992137814f43b909e3397a2d80
armandodaves <armandodavidesle067@gmail.com>
2024-02-29 13:22:37 -0500

    Segunda version, nueva linea d ecodigo implementada

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,7 @@
 def welcome_message():
     name = input('Ingrese su nombre: ')
     print(f'Bienvenido {name} al curso de Django y React')
+    print('Esta es una nueva línea de código')
     
 if __name__ == "__main__":
     welcome_message()
